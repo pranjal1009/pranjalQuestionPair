3e91bd0af860a3826702af79aa74ebbb57e1954e
pranjal1009 <90826977+pranjal1009@users.noreply.github.com>
2023-11-30 19:14:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,6 @@ if st.button('Find'):
     result = model.predict(query)[0]
 
     if result:
-        st.subheader('Questions are :rainbow[Duplicate]')
+        st.subheader('Questions are :violet[Duplicate]')
     else:
-        st.subheader('Questions are :rainbow[Not Duplicate]')
+        st.subheader('Questions are :violet[Not Duplicate]')
